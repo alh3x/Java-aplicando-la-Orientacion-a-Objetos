05b295774273146cc3406f30eef07c53ca3405d0
alex <oliver-pinto@outlook.com>
2024-04-15 15:02:28 -0300

    aula2 finalizada

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,33 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Blade Runner";
-        miPelicula.fechaDeLanzamiento = 1982;
-        miPelicula.duracionEnMinutos = 117;
+        miPelicula.setNombre("Blade Runner");
+        miPelicula.setFechaDeLanzamiento(1982);
+        miPelicula.setDuracionEnMinutos(117);
+        miPelicula.setIncluidoEnPlan(true);
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(8.9);
         miPelicula.evalua(9.8);
-
-        System.out.println(miPelicula.sumaDeLasEvaluaciones);
-        System.out.println(miPelicula.totalEvaluaciones);
+        miPelicula.evalua(7.6);
+        System.out.println(miPelicula.getTotalEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-        Pelicula miPelicula2 = new Pelicula();
-        miPelicula2.nombre = "Matrix";
-        miPelicula2.fechaDeLanzamiento = 2000;
-        miPelicula2.duracionEnMinutos = 120;
 
+
+
+
+
+
+
+
+
+        //com.aluracursos.screenmatch.modelos.Pelicula miPelicula2 = new com.aluracursos.screenmatch.modelos.Pelicula();
+        //miPelicula2.nombre = "Matrix";
+        //miPelicula2.fechaDeLanzamiento = 2000;
+        //miPelicula2.duracionEnMinutos = 120;
         //miPelicula2.muestraFichaTecnica();
-
-
     }
 }
---
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,61 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Pelicula {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnPlan;
+    private double sumaDeLasEvaluaciones;//
+    private int totalEvaluaciones;//solo podemos manipular estos atributos en esta clase
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnPlan() {
+        return incluidoEnPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;//this se refiere al atributo de esta clase
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
+        this.incluidoEnPlan = incluidoEnPlan;
+    }
+
+    public int getTotalEvaluaciones() {
+        return totalEvaluaciones;
+    }
+
+    public void muestraFichaTecnica() {
+        System.out.println("El nombre de la pelicula es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos: " + duracionEnMinutos);
+    }
+
+    public void evalua(double nota) {
+        sumaDeLasEvaluaciones += nota;
+        totalEvaluaciones++;
+    }
+
+    public double calculaMedia() {
+        return sumaDeLasEvaluaciones / totalEvaluaciones;
+    }
+}
